785426a27f022d0b9ab069a0e159ed302ec20de5
Diseurs Matteo <diseursmat@gmail.com>
2026-07-27 20:59:59 +0200

    fix: refactor image paths in ProjectSection.vue to use dynamic import function

--- src/components/project/ProjectSection.vue
+++ src/components/project/ProjectSection.vue
@@ -1,31 +1,34 @@
 <script setup>
 import ProjectCard from './ProjectCard.vue'
+
+const img = (path) => `${import.meta.env.BASE_URL}images/${path}`
+
 const projects = [
   {
     title: 'Portfolio',
     description: 'Mon portfolio personnel pour présenter mes projets et compétences.',
-    image: '/images/portfolio.png',
+    image: img('portfolio.png'),
     type: 'Web Development',
     link: 'https://github.com/matteodiseurs/portfolio'
   },
   {
     title: 'Airsoft Pro Shop',
     description: 'Un site e-commerce pour la vente d\'équipements d\'airsoft, avec une analyse de marché, concurence, proto-persona et wireframes.',
-    image: '/images/airsoft.png',
+    image: img('airsoft.png'),
     type: 'E-Buisness / Analyse',
     link: 'https://github.com/matteodiseurs/airsoft-pro-shop'
   },
   {
     title: 'Projet TicketMaster',
     description: 'Un système de ticket pour gérer les demandes de support, avec une interface utilisateur simple utilisant Twig, Boostrap, php & MySQl.',
-    image: './public/images/TicketSystem.png',
+    image: img('ticketsystem.png'),
     type: 'Web Development / Database Design',
     link: 'https://github.com/LoKiass/ProjetTicketIT',
   },
   {
     title: 'Examen Administration & Gestion de Base de Données Avancée',
     description: 'Un projet d\'examen pour la création d\'une base de données avancée, avec des requêtes SQL complexes et des relations entre les tables.',
-    image: '/images/examendb.png',
+    image: img('examendb.png'),
     type: 'Database Design',
     link: 'https://github.com/LoKiass/EAFCSUDLUX2025-2026_EXAMENDB'
   }
