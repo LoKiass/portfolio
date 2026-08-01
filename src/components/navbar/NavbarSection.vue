49067a0f96dc364e79f92829d1b943911c62769a
Diseurs Matteo <diseursmat@gmail.com>
2026-08-01 14:03:36 +0200

    fix: update NavbarSection.vue for improved layout and styling; enhance accessibility with responsive text sizes

--- src/components/navbar/NavbarSection.vue
+++ src/components/navbar/NavbarSection.vue
@@ -3,13 +3,29 @@
 </script>
 
 <template>
-  <nav class="p-4 shadow-md border-b-2 border-dark-40">
-    <ul class="flex gap-6 font-bold text-white">
-      <li><a href="#About" class=" transition"><span class="bg-linear-to-r from-cyan-500 via-cyan-600 to-cyan-700 bg-clip-text text-transparent">MD</span>|Mattéo Diseurs</a></li>
-      <li class="ml-auto"><a href="#About" class="hover:text-blue-200 hover:underline transition ">À propos</a></li>
-      <li><a href="#TechStack" class="hover:text-blue-200 hover:underline transition">Stack technique</a></li>
-      <li><a href="#Projects" class="hover:text-blue-200 hover:underline transition">Projet</a></li>
-      <li><a href="https://github.com/LoKiass" class=""><img src="/images/github-white-icon.png" alt="GitHub" class="w-6 h-6" /></a></li>
+  <nav class="p-4 shadow-md border-b border-gray-700">
+    <ul class="flex items-center gap-4 sm:gap-6 font-bold text-white">
+      <li>
+        <a href="#About" class="transition">
+          <span class="bg-linear-to-r from-cyan-500 via-cyan-600 to-cyan-700 bg-clip-text text-transparent">MD</span>
+          <span class="hidden sm:inline"> | Mattéo Diseurs</span>
+        </a>
+      </li>
+
+      <li class="ml-auto text-sm sm:text-base">
+        <a href="#About" class="hover:text-cyan-400 transition">À propos</a>
+      </li>
+      <li class="text-sm sm:text-base">
+        <a href="#TechStack" class="hover:text-cyan-400 transition">Stack</a>
+      </li>
+      <li class="text-sm sm:text-base">
+        <a href="#Projects" class="hover:text-cyan-400 transition">Projets</a>
+      </li>
+      <li class="flex items-center">
+        <a href="https://github.com/LoKiass" target="_blank" rel="noopener noreferrer">
+          <img src="/images/github-white-icon.png" alt="GitHub" class="w-6 h-6 min-w-6" />
+        </a>
+      </li>
     </ul>
   </nav>
 </template>
